39d29c15ae0a00210ba6a33398f6650706a88670
Maicol Castro <maicolestivencastromoreno525@gma8il.com>
2026-03-03 17:19:13 -0500

    Configuracion puerto render

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.get("/sacar", (req, res) => {
 
 });
 
-app.listen(5000, () => {
-    console.log("Servidor corriendo en http://localhost:5000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto " + PORT);
 });
